4774ae11817fb613ffda01749b5970338cba6277
MaksudSharipov <maqsbek72@gmail.com>
2022-03-26 15:58:00 +0500

    first version

---
+++ UzbekLemmatizer.py
@@ -0,0 +1,9 @@
+def Lemma(word):
+    # call FSM(noun-tur)
+    # call FSM(verb)
+    # call FSM(ajectiv)
+    # call FSM(adverb)
+    # call FSM(number)
+    # call FSM(pronouns)
+    return word
+
